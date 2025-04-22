9f292f25417b269ddee6eedb6c1f17438e360324
george-lk <georgealt88@gmail.com>
2025-04-22 17:08:38 +0800

    Code cleanup

--- lua/current-workspace-info/init.lua
+++ lua/current-workspace-info/init.lua
@@ -86,8 +86,13 @@ function ret_func.show_current_scope()
     vim.api.nvim_buf_add_highlight(window_buffer, 0, "custom_current_workspace_info_highlight_info_title",9, 0, -1)
     vim.api.nvim_buf_add_highlight(window_buffer, 0, "custom_current_workspace_info_highlight_info_title",12, 0, -1)
 
-    local close_cmd_window = '<Cmd>lua vim.api.nvim_set_current_win(' .. user_current_window .. '); <CR>'
-    vim.api.nvim_buf_set_keymap(window_buffer,'n','<Esc>',close_cmd_window, {noremap=true, silent=true})
+    vim.keymap.set('n', '<Esc>',
+        function()
+            vim.api.nvim_set_current_win(user_current_window)
+        end,
+        {buffer = window_buffer}
+    )
+
     local au_workspace_info_id = vim.api.nvim_create_augroup("au_workspace_info", {clear = true})
     local autocmd_buf_enter_buf = vim.api.nvim_create_autocmd(
         "BufEnter",
